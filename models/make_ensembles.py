72930bbbd3e945036bf05dd8c8f99d81fded23f7
pciturri <pciturri@gfz-potsdam.de>
2024-07-25 11:55:51 +0200

    added emec catalog and declustered version

---
+++ models/make_ensembles.py
@@ -0,0 +1,53 @@
+import csep
+import oq2csep
+import os
+
+
+lt_eshm13 = [('eshm13_as.csv', 0.5),  # to do real vals
+             ('eshm13_fsbg.csv', 0.3),
+             ('eshm13_seifa.csv', 0.2)]
+
+# component weights
+uas = 0.5
+fssm = 0.5
+
+tr = 0.6
+abl = 0.2
+abm= 0.6
+abu = 0.2
+
+ml = 0.5
+ma = 0.4
+mu = 0.1
+
+paretto = 0.4
+
+srl = 0.1
+sra = 0.5
+sru = 0.4
+
+weights = [('ABL_ML.csv', uas*tr*abl*ml),
+           ('ABL_MC.csv', uas*tr*abl*ma),
+           ('ABL_MU.csv', uas*tr*abl*mu),
+           ('ABM_ML.csv', uas*tr*abm*ml),
+           ('ABM_MC.csv', uas*tr*abm*ma),
+           ('ABM_MU.csv', uas*tr*abm*mu),
+           ('ABU_ML.csv', uas*tr*abu*ml),
+           ('ABU_MC.csv', uas*tr*abu*ma),
+           ('ABU_MU.csv', uas*tr*abu*mu),
+           ('PM_ML.csv', uas*paretto*ml),
+           ('PM_MC.csv', uas*paretto*ma),
+           ('PM_MU.csv', uas*paretto*mu),
+           ('fs_SRL_ML.csv', fssm*srl*ml),
+           ('fs_SRL_MA.csv', fssm*srl*ma),
+           ('fs_SRL_MU.csv', fssm*srl*mu),
+           ('fs_SRA_ML.csv', fssm*sra*ml),
+           ('fs_SRA_MA.csv', fssm*sra*ma),
+           ('fs_SRA_MU.csv', fssm*sra*mu),
+           ('fs_SRU_ML.csv', fssm*sru*ml),
+           ('fs_SRU_MA.csv', fssm*sru*ma),
+           ('fs_SRU_MU.csv', fssm*sru*mu)]
+
+
+# for comp in lt_eshm13:
+#     oq2csep.(comp[0], comp[1])
